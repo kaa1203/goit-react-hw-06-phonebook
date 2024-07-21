5fdd21a000c6a3b3752a92087f4bcbfb2ddd868b
kaa1203 <kennethaguila1203@gmail.com>
2024-07-21 21:29:21 +0800

    Update Search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,17 +1,11 @@
 import css from "./Search.module.css";
 import { searchValue } from "../../redux/searchSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { getSearch } from "../../redux/selector";
+import { useDispatch } from "react-redux";
 
 export const Search = () => {
    const dispatch = useDispatch();
-   const search = useSelector(getSearch);
 
-   const handleOnChange = e => {
-      // const searchValue = contacts.filter(contact => contact.name.toLowerCase().includes(search.toLowerCase()));
-      // console.log(searchValue);
-      dispatch(searchValue(e.target.value))
-   }
+   const handleOnChange = e => dispatch(searchValue(e.target.value));
 
    return (
       <div className={css.searchWrapper}>
